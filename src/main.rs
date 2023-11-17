17ceaf83d386475e5cce90b48bff38f8f280e090
randych-gilad <xadaka@gmail.com>
2023-11-17 19:47:54 +0200

    early inventory

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
+const INVENTORY_SIZE: usize = 9;
+const WEIGHT_CAP: u8 = 50;
+#[derive(Clone)]
+struct Inventory(Vec<Item>);
+
+#[derive(Clone, Eq, PartialEq)]
+struct Item {
+    name: &'static str,
+    weight: u8,
+}
+
 fn main() {}
