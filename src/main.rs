ba99acf7df45e28e2ff05aa8c1a33f7ab22c051f
randych-gilad <xadaka@gmail.com>
2023-11-17 19:48:33 +0200

    early impl inventory

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,15 @@ struct Item {
     name: &'static str,
     weight: u8,
 }
-
+impl Inventory {
+    fn add_item(&mut self, item: Item) {
+        match self.0.len() {
+            ..=INVENTORY_SIZE => {
+                println!("Added {} at slot {}.", item.name, self.0.len() + 1);
+                self.0.push(item);
+            }
+            _ => println!("Cannot add {}. Inventory is full.", item.name),
+        }
+    }
+}
 fn main() {}
